791917c7a77ce4949684d69ccc11589b25f1aac3
kerem <bftrade07@gmail.com>
2025-07-18 10:38:56 +0300

    stable
    added json based cooldown system,
    added some commands like ping, purge,
    fixed the message of "you should wait x hours and x minutes"

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import random
 import time
 import os
+import sys
 import json
 from discord.ext import commands
 from discord.ext.commands import CommandOnCooldown
@@ -11,15 +12,50 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+if os.path.exists("cooldowns.json"):
+    with open("cooldowns.json", "r") as f:
+        try:
+            cooldowns = json.load(f)
+        except json.JSONDecodeError:
+            cooldowns = {}
+else:
+    cooldowns = {}
+
+
 @bot.event
 async def on_ready():
-    print(f"The bot is logged as {bot.user}")
+    print(f"{bot.user} is ready!")
 
 @bot.command()
-@commands.cooldown(1, 7200, commands.BucketType.user)  # 1 kullanım, 7200 saniye (2 saat), kullanıcı bazında
-async def dick(ctx, option: str = None):
+@commands.is_owner()
+async def reload(ctx):
+    await ctx.send("🔁 reloading...")
+    os.execv(sys.executable, ['python'] + sys.argv)
+
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def purge(ctx, miktar):
+    sil = await ctx.channel.purge(limit=int(miktar))
+
+
+@bot.command()
+async def ping(ctx):
+    '''
+    This text will be shown in the help command
+    '''
+
+    # Get the latency of the bot
+    latency = bot.latency  # Included in the Discord.py library
+    # Send it to the user
+    await ctx.send(f"pong! {latency}ms")
+
+
+@bot.command()
+async def sik(ctx, option: str = None):
     user_id = str(ctx.author.id)
-    
+
+    # Dosyadan veriyi oku, yoksa boş dict yap
     if os.path.exists("dicks.json"):
         with open("dicks.json", "r") as f:
             try:
@@ -29,53 +65,94 @@ async def dick(ctx, option: str = None):
     else:
         data = {}
 
-    buyume_rakamlari = list(range(1,13))
-    secilen_buyume = random.choice(buyume_rakamlari)
-    boy = data.get(user_id, 0)
+    # Eğer option parametresi yoksa uyar
+    if option is None:
+        await ctx.send("these are the only options that you can use: grow / howmanycm / top")
+        return
 
+    # cooldown sadece 'buyut' seçeneğinde geçerli
     if option == "grow":
+        now = time.time()
+        cooldown_time = 7200  # 2 saat = 7200 saniye
+        last_time = cooldowns.get(user_id, 0)
+
+        if now - last_time < cooldown_time:
+            kalan = int(cooldown_time - (now - last_time))
+            saat = int(kalan/3600)
+            dakika = int((kalan%3600) / 60)
+            user = ctx.author.mention
+            
+            if saat >= 1:
+                await ctx.send(f"{user}, you can grow your dick after {saat} hours {dakika} minutes")
+                return
+
+            if saat == 0:
+                await ctx.send(f"{user}, you can grow your dick after {dakika} minutes")
+                return
+
+        else:
+            cooldowns[user_id] = now
+            with open("cooldowns.json", "w") as f:
+                json.dump(cooldowns, f, indent=4)
+
+
+        buyume_rakamlari = list(range(1, 13))
+        secilen_buyume = random.choice(buyume_rakamlari)
+        boy = data.get(user_id, 0)
         boy += secilen_buyume
         data[user_id] = boy
 
         with open("dicks.json", "w") as f:
             json.dump(data, f, indent=4)
 
-        await ctx.send(f"{ctx.author.mention}, ur dick is grew up {secilen_buyume}cm! : {boy} cm 🍆")
+        await ctx.send(
+            f"{ctx.author.mention}, your dick is grew up {secilen_buyume}cm, now its: {boy}cm!"
+        )
+        return
 
-
-    if option == "howmanycm":
-        await ctx.send(f"ur dick is {boy}cm {ctx.author.mention}-san1!1!!")
+    # cooldown yok, diğer seçenekler:
+    if option == "kaccm":
+        boy = data.get(user_id, 0)
+        await ctx.send(f"{ctx.author.mention}, your dick is {boy} cm!")
         return
 
     if option == "top":
         if not data:
-            await ctx.send("there is no data")
+            await ctx.send("no one has a dick lmao lol ts pmo mdr")
             return
 
         sirali = sorted(data.items(), key=lambda x: x[1], reverse=True)
 
-        mesaj = "**server top dick size:**\n"
+        mesaj = "**leaderboard of dicks:**\n"
         for i, (uid, boy) in enumerate(sirali, start=1):
             try:
                 user = await ctx.guild.fetch_member(int(uid))
-                isim = user.display_name
+                isim = user.display_name if user else f"User ID: {uid}"
             except:
                 isim = f"User ID: {uid}"
 
             mesaj += f"{i}. {isim} — {boy} cm\n"
 
-        if len(mesaj) > 2000:
-            mesaj = mesaj[:1990] + "\n...ve devamı var."
+            if len(mesaj) > 1900:
+                mesaj += "\n...ve devamı var."
+                break
 
         await ctx.send(mesaj)
         return
 
+    # Bilinmeyen seçenek
+    await ctx.send(
+        "these are the only options that you can use: grow / howmanycm / top"
+    )
+
+
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, CommandOnCooldown):
-        await ctx.send(f"{ctx.author.mention}, you need to wait {round(error.retry_after)} seconds to use this command.")
+        await ctx.send(
+            f"{ctx.author.mention}, you should wait {round(error.retry_after)/60} to use this command"
+        )
     else:
         raise error  # Diğer hataları normal şekilde göster
-
 
 bot.run("INSERT YOUR TOKEN HERE")
